a59494c268b3f3c1ce547c2fa5174db2c8631936
Victor-Graca <vctr.oliveira09@gmail.com>
2023-09-23 11:30:36 -0300

    Repositorio 1

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import Table from './Table'
+import React, {Component} from 'react'
+import Form from './Form'
+
+class App extends Component {
+  removerAluno = (index) => {
+    const{alunos} = this.state
+    this.setState(
+      {
+        alunos: alunos.filter((aluno, i) => {
+          return i !== index
+        })
+      }
+    )
+  }
+
+  handleSubmit = (alunoDisc) => {
+    this.setState({
+      alunos: [...this.state.alunos, alunoDisc]
+    })
+  }
+
+  state = {
+    alunos: [
+      /*
+      {
+        nome: 'Dudu',
+        turma: 'Programação Web',
+      },
+      {
+        nome: 'Paty',
+        turma: 'Análise de Algoritmos',
+      },
+      {
+        nome: 'Joãozinho',
+        turma: 'Programação 00',
+      },
+      {
+        nome: 'Ju',
+        turma: 'Engenharia de Requisitos',
+      }
+      */
+    ]
+  }
+  render(){
+  const {alunos} = this.state
+  return (
+    <div className="container">
+      <Table alunos = {alunos}
+          removerAluno={this.removerAluno}/> 
+      <Form handleSubmit={this.handleSubmit}/>    
+    </div>
+  );
+  }
+}
+
+
+export default App;
